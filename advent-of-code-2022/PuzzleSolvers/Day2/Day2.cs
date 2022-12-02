36ce3031bb819398fa4bdc3e0cc9935ed727f2d3
Broxxar <danjohnmoran@gmail.com>
2022-12-02 11:25:26 -0800

    Day 2!

---
+++ advent-of-code-2022/PuzzleSolvers/Day2/Day2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    [PuzzleDay(2)]
+    public class Day2 : IPuzzleSolver
+    {
+        private Dictionary<string, int> p1 = new Dictionary<string, int>
+        {
+            { "A X", 1 + 3 },
+            { "A Y", 2 + 6 },
+            { "A Z", 3 + 0 },
+            { "B X", 1 + 0 },
+            { "B Y", 2 + 3 },
+            { "B Z", 3 + 6 },
+            { "C X", 1 + 6 },
+            { "C Y", 2 + 0 },
+            { "C Z", 3 + 3 }
+        };
+
+        private Dictionary<string, int> p2 = new Dictionary<string, int>
+        {
+            { "A X", 0 + 3 },
+            { "A Y", 3 + 1 },
+            { "A Z", 6 + 2 },
+            { "B X", 0 + 1 },
+            { "B Y", 3 + 2 },
+            { "B Z", 6 + 3 },
+            { "C X", 0 + 2 },
+            { "C Y", 3 + 3 },
+            { "C Z", 6 + 1 }
+        };
+
+        public int PartOne(string input) => input.Split('\n').Select(l => p1[l]).Sum();
+        public int PartTwo(string input) => input.Split('\n').Select(l => p2[l]).Sum();
+    }
+}
---
+++ advent-of-code-2022/PuzzleSolvers/Day2/readme.md
@@ -0,0 +1,7 @@
+﻿# Day 2
+
+Cheating at Rock, Paper, Scissors seems like a good way to end up on the naughty list, but oh well!
+
+My solution to the puzzle also felt a bit like cheating. I hummed and hawed for a minute before thinking I could just write out the solution pretty quickly in a Dictionary. With the 9 possible outcomes mapped out, the solution is trivial. Part Two was a matter of shuffling the numbers around to account for the change of rules.
+
+Wasn't thrilled with my solve, but I managed to submit in 10:26 for a rank of 876. Having all the boilerplate set up so I can just implement the functions and run my code helped a lot, but if I want to get more competitive, I might need solutions which are slightly more clever than hard coding the answers! 😅
