023ef520269d03cbecd2027f4ffab538c5870928
Broxxar <danjohnmoran@gmail.com>
2022-12-03 10:43:58 -0800

    Odds and ends to help with future puzzles.

---
+++ advent-of-code-2022/Math/int2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AdventOfCode
+{
+    [StructLayout(LayoutKind.Explicit, Size = 8)]
+    public struct int2
+    {
+        [FieldOffset(0)] private unsafe fixed int _data[2];
+        [FieldOffset(0)] public int x;
+        [FieldOffset(4)] public int y;
+
+        public unsafe int this[int i]
+        {
+            get => _data[i];
+            set => _data[i] = value;
+        }
+
+        public int2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static int2 operator +(int2 a, int2 b)
+        {
+
+            return new int2(a.x + b.x, a.y + b.y);
+        }
+
+        public static int2 operator -(int2 a, int2 b)
+        {
+            return new int2(a.x - b.x, a.y - b.y);
+        }
+
+        public static int2 operator *(int2 a, int2 b)
+        {
+            return new int2(a.x * b.x, a.y * b.y);
+        }
+
+        public static int2 operator /(int2 a, int2 b)
+        {
+            return new int2(a.x / b.x, a.y / b.y);
+        }
+
+        public static bool operator ==(int2 a, int2 b)
+        {
+            return a.x == b.x && a.y == b.y;
+        }
+
+        public static bool operator !=(int2 a, int2 b)
+        {
+            return !(a == b);
+        }
+        
+        public static int2 Max(int2 a, int2 b)
+        {
+            return new int2(Math.Max(a.x, b.x), Math.Max(a.y, b.y));
+        }
+
+        public static int2 Min(int2 a, int2 b)
+        {
+            return new int2(Math.Min(a.x, b.x), Math.Min(a.y, b.y));
+        }
+
+        public static int2 Sign(int2 p)
+        {
+            return new int2(Math.Sign(p.x), Math.Sign(p.y));
+        }
+
+        public static int2 Parse(string str, char separator = ',')
+        {
+            var s = str.Split(separator);
+            return new int2(s[0].Int(), s[1].Int());
+        }
+
+        public bool Equals(int2 other)
+        {
+            return x == other.x && y == other.y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is int2 other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
+        }
+
+        public override string ToString() => $"({x}, {y})";
+    }
+}
--- advent-of-code-2022/Utility/GolfHelpers.cs
+++ advent-of-code-2022/Utility/GolfHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -10,6 +11,18 @@ namespace AdventOfCode
     {
         public static int Int(this string str) => int.Parse(str);
 
+        public static int2 Int2(this string str, char separator = ',') => int2.Parse(str, separator);
+
+        public static string Replace(this string str, params (string oldValue, string newValue)[] replacements)
+        {
+            foreach (var replacement in replacements)
+            {
+                str = str.Replace(replacement.oldValue, replacement.@newValue);
+            }
+
+            return str;
+        }
+
         public static bool Bool(this string str) => bool.Parse(str);
 
         public static IEnumerable<T> Insert<T>(this IEnumerable<T> enumerable, int index, T value)
@@ -17,6 +30,14 @@ namespace AdventOfCode
             var list = enumerable.ToList();
             list.Insert(index, value);
             return list;
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> functor)
+        {
+            foreach (var element in enumerable)
+            {
+                functor(element);
+            }
         }
     }
 }
