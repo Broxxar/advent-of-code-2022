bd8edea8655a7cc8c4030544ac36e58726f9aab0
Broxxar <danjohnmoran@gmail.com>
2022-12-06 14:23:48 -0800

    Subtle bugaboo, didn't actually matter because it would only crash if the puzzle input did not contain the start-of-message markerí¸! �

--- advent-of-code-2022/PuzzleSolvers/Day6/Day6.cs
+++ advent-of-code-2022/PuzzleSolvers/Day6/Day6.cs
@@ -6,6 +6,6 @@ namespace AdventOfCode.Day6
     public class Day6 : IPuzzleSolver
     {
         public int PartOne(string input) => (..(input.Length - 4)).Itr().First(i => input[i..(i + 4)].Set().Count == 4) + 4;
-        public int PartTwo(string input) => (..(input.Length - 4)).Itr().First(i => input[i..(i + 14)].Set().Count == 14) + 14;
+        public int PartTwo(string input) => (..(input.Length - 14)).Itr().First(i => input[i..(i + 14)].Set().Count == 14) + 14;
     }
 }
